91ae8371efd7456db1cc08d3bf6f9f7eea8c906f
solomoreal <solomoreal@yahoo.com>
2017-11-23 20:00:02 +0100

    added a login page for partners, a php 'include' folder and change of extensions from .php to .html

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php
+	defined('DNS') ? null : define ('DNS', 'mysql:host=localhost;dbname=assualt_app;charset=utf8');
+	
+	defined('DB_USER') ? null : define ('DB_USER','root');
+
+	defined('DB_PASS') ? null : define ('DB_PASS','');
+
+?>
---
+++ include/session.php
@@ -0,0 +1,40 @@
+<?php 
+	class Session{
+		public $user_id;
+		private $logged_in;
+
+		function __construct(){
+			session_start();
+			$this->check_login();
+		}
+
+		public function check_login(){
+			if(isset($_SESSION['user_id'])){
+				$this->user_id = $_SESSION['user_id'];
+				$this->logged_in = true;
+				}else {
+					unset($this->user_id);
+					$this->logged_in = false;
+				}
+			}	
+
+			public function is_logged_in(){
+				return $this->logged_in;
+			}
+			
+			public function login($user){
+				if ($user) {
+					$this->user_id = $_SESSION['user_id'] = $user->getStaffId();
+					$this->logged_in = true;
+				}
+			}
+
+			public function logout(){
+				//$this->user_id = null;
+				$this->logged_in = false;
+				session_destroy();
+			}
+		}
+	
+	$session = new session();
+?>
